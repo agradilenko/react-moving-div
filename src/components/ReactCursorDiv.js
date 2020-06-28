5cfbf92b59a5c755cc28fc1cb9ea3af2937cf242
agradilenko <44676263+agradilenko@users.noreply.github.com>
2020-06-28 18:10:33 +0300

    Initial commit

---
+++ src/components/ReactCursorDiv.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+export default class ReactCursorDiv extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      x: 0,
+      y: 0
+    };
+    this._onMouseMove = this._onMouseMove.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousemove', this._onMouseMove);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousemove', this._onMouseMove);
+  }
+
+  _onMouseMove(e) {
+    this.setState({
+      x: e.clientX,
+      y: e.clientY
+    });
+  }
+
+  render() {
+    const { x, y } = this.state;
+    const { style, offsetX, offsetY, ...rest } = this.props;
+    return (
+      <div
+        {...rest}
+        onMouseMove={this._onMouseMove.bind(this)}
+        style={
+          {
+            position: "fixed",
+            left: x + (offsetX || 0),
+            top: y + (offsetY || 0),
+            backgroundColor: "black",
+            color: "white"
+        } }
+      />
+    );
+  }
+}
+
